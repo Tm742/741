9bfc5c0a4fc0f626965af9cba36c484013413ba3
Tm_741 <294614664@qq.com>
2020-07-03 20:11:33 +0800

    个人中心

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="login-container">
     <!-- 导航栏 -->
-    <van-nav-bar class="page-nav-bar" title="登录" />
+    <van-nav-bar class="page-nav-bar" title="登录">
+    <van-icon slot="left" name="cross" @click="$router.back()" />
+      </van-nav-bar>
     <!-- /导航栏 -->
 
     <!-- 登录表单 -->
@@ -118,6 +120,8 @@ export default {
         this.$store.commit('setUser', data.data)
         // console.log('登录成功', res)
         this.$toast.success('登录成功')
+        // 登陆成功，跳转回原来页面
+        this.$router.back()
       } catch (err) {
         if (err.response.status === 400) {
           // console.log('手机号或验证码错误')

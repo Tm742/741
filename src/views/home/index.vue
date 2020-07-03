d7b5bae0aa4a9d519a1c1810068cbd34da9a2d68
Tm_741 <294614664@qq.com>
2020-07-03 21:32:05 +0800

    首页-处理文章频道列表-样式调整

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button
+        class="search-btn"
+        slot="title"
+        type="info"
+        size="small"
+        round
+        icon="search"
+        >搜索</van-button
+      >
+    </van-nav-bar>
+    <!-- /导航栏 -->
+
+    <!-- 频道列表 -->
+    <van-tabs class="channel-tabs" v-model="active" animated swipeable>
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
+    <!-- /频道列表 -->
+  </div>
 </template>
 
 <script>
@@ -8,7 +36,9 @@ export default {
   components: {},
   props: {},
   data() {
-    return {}
+    return {
+      active: 0
+    }
   },
   computed: {},
   watch: {},
@@ -18,4 +48,44 @@ export default {
 }
 </script>
 
-<style scope lang="less"></style>
+<style scope lang="less">
+.home-container {
+  .van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    .van-icon {
+      font-size: 32px;
+    }
+  }
+
+  /deep/ .channel-tabs {
+    .van-tabs__wrap {
+      height: 82px;
+    }
+    .van-tab {
+      min-width: 200px;
+      border-right: 1px solid #edeff3;
+      font-size: 30px;
+      color: #777777;
+    }
+    .van-tab--active {
+      color: #333333;
+    }
+    .van-tabs__nav {
+      padding-bottom: 0;
+    }
+    .van-tabs__line {
+      bottom: 8px;
+      width: 31px !important;
+      height: 6px;
+      background-color: #3296fa;
+    }
+  }
+}
+</style>
